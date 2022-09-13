58b8b3a1fd8eca415d5d097a096da4d162ccb027
Jervaro <jeremyvaudan@hotmail.com>
2022-09-13 21:56:04 +0200

    Added cooldown for shooting

--- Assets/Scripts/Weapon.cs
+++ Assets/Scripts/Weapon.cs
@@ -6,19 +6,33 @@ public class Weapon : MonoBehaviour
 {
     public Transform firePoint;
     public GameObject bulletPrefab;
+    private bool canFire = true;
+    [SerializeField] private float timeBeforeReset;
+    private float timer = 0f;
 
     // Update is called once per frame
     void Update()
     {
-        if(Input.GetButtonDown("Fire1"))
+        if(Input.GetButtonDown("Fire1") && canFire == true)
         {
             Shoot();
+        }
+
+        if (canFire == false)
+        {
+            timer += Time.deltaTime;
+            if (timer >= timeBeforeReset)
+            {
+                canFire = true;
+                timer = 0f;
+            }
         }
     }
 
     void Shoot()
     {
         Instantiate(bulletPrefab, firePoint.position, firePoint.rotation);
-        
+        canFire = false;
     }
+    
 }
