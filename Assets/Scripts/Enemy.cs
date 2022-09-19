a795ee81447ac3b476ac9311e40fa46284beb2fe
AZURRIANNIIR <113354208+AZURRIANNIIR@users.noreply.github.com>
2022-09-19 11:03:24 +0200

    IntrolevelChanges

--- Assets/Scripts/Enemy.cs
+++ Assets/Scripts/Enemy.cs
@@ -13,8 +13,10 @@ public class Enemy : MonoBehaviour
     public void TakeDamage(int damage)
     {
         health -= damage;
+       
         if (health <= 0)
         {
+           
             enemySlimeMovement.KillMe();
         }
     }
