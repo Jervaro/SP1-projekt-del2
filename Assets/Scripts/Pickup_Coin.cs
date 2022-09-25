7ec957e5268e2dd993e8836306293f9ebd66322c
Jervaro <jeremyvaudan@hotmail.com>
2022-09-25 18:01:35 +0200

    changed Pickup_Coin script and Bullet script
    changed so coin objects are destroyed after certain amount of time.
    Tried to add impact effect to bullet script but removed it

--- Assets/Scripts/Pickup_Coin.cs
+++ Assets/Scripts/Pickup_Coin.cs
@@ -9,12 +9,13 @@ public class Pickup_Coin : MonoBehaviour
 
     [SerializeField] private AudioSource audioSource;
     [SerializeField] private AudioClip pickupClip;
-
+    [SerializeField] private GameObject coinPrefab;
+    [SerializeField] private float timeBeforeDeletion = 1f;
     private bool canPickupCoin = true;
 
     private bool removeGameObject = false;
-    private float timer = 0f;
-    [SerializeField] private float timeBeforeDeletion = 1f;
+    //private float timer = 0f;
+
 
     private void Update()
     {
@@ -34,7 +35,14 @@ public class Pickup_Coin : MonoBehaviour
                 canPickupCoin = false;
                 audioSource.pitch = Random.Range(0.9f, 1.1f);
                 audioSource.PlayOneShot(pickupClip);
+                Invoke("DestroyCoin", timeBeforeDeletion);
             }
         }
     }
+
+    void DestroyCoin()
+    {
+        Destroy(coinPrefab);
+    }
+
 }
